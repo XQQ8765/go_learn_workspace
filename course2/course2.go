6eda8c543590af310f77dd5738b9c5b968e4ff20
Rabbit Xiao (rxiao) <rabbit.xiao@quest.com>
2020-01-29 13:52:23 +0800

    first commit

---
+++ course2/course2.go
@@ -0,0 +1,76 @@
+package main
+import (
+	"fmt"
+	"reflect"
+)
+
+//结构体
+type people struct {
+	age int
+	name string
+}
+
+//九九go语言入门教程 2
+//https://v.youku.com/v_show/id_XMzI4MDA0MDc5Mg==.html?spm=a2h0j.11185381.listitem_page1.5~A
+func main() {
+	fmt.Println("Course 2.")
+	var vInt int = 5
+	fmt.Println(vInt)
+	fmt.Println(vInt, reflect.TypeOf(vInt))
+
+	var vInt32 int32 = 5
+	fmt.Println(vInt32, reflect.TypeOf(vInt32))
+
+	var vInt64 int64 = 500000001000
+	fmt.Println(vInt64, reflect.TypeOf(vInt64))
+
+	var vByte byte = 5
+	//"byte" is with type "uint8", 0-255, 无负数
+	fmt.Println(vByte, reflect.TypeOf(vByte))
+
+	var vString string = "hello"
+	fmt.Println(vString, reflect.TypeOf(vString))
+
+	var vArra [3]int = [3]int{}
+	vArra[0] = 3
+	vArra[2] = 5
+	fmt.Println(vArra, reflect.TypeOf(vArra))
+
+	//可变数组
+	var vSlice []int
+	fmt.Println(vSlice, reflect.TypeOf(vSlice))
+	//vSlice[0] = 1//Error: runtime error: index out of range [0] with length 0
+	fmt.Println(vSlice == nil)//true, 没初始化
+
+	var vSlice2 []int = make([]int, 2)//初始化可变数组
+	fmt.Println(vSlice2, reflect.TypeOf(vSlice2))
+	fmt.Println(vSlice2 == nil)//false
+
+	var vSlice3 []int = make([]int, 2, 10)//初始化可变数组,可拓展
+	vSlice3[1] = 1
+	fmt.Println(vSlice3, reflect.TypeOf(vSlice3), len(vSlice3), cap(vSlice3))
+
+	var vArraStr [3]string = [3]string{}
+	vArraStr[1] = "bb"
+	fmt.Println(vArraStr, reflect.TypeOf(vArraStr))
+
+	//map
+	var vMap map[int]int
+	fmt.Println(vMap, reflect.TypeOf(vMap), vMap == nil)
+
+	var vMap2 map[int]int = make(map[int]int)
+	fmt.Println(vMap2, reflect.TypeOf(vMap2), vMap2 == nil)
+	vMap2[2] = 6
+	vMap2[3] = 9
+	fmt.Println(vMap2)
+	fmt.Println(vMap2[0])//0
+
+	var vstrMap = make(map[string]int)
+	vstrMap["apple"] = 5
+	fmt.Println(vstrMap, reflect.TypeOf(vstrMap), vstrMap == nil)
+	fmt.Println(vstrMap["apple"])
+
+	//结构体
+	var vPoint *people = new(people)
+	fmt.Println(vPoint, reflect.TypeOf(vPoint), vPoint == nil)
+}
---
+++ tmp.groovy
@@ -0,0 +1,92 @@
+go.toolsGopath setting is not set. Using GOPATH D:\GoPath
+Installing 15 tools at D:\GoPath\bin in module mode.
+  gopkgs
+  go-outline
+  go-symbols
+  guru
+  gorename
+  gotests
+  gomodifytags
+  impl
+  fillstruct
+  goplay
+  godoctor
+  gocode-gomod
+  godef
+  goreturns
+  golint
+
+Installing github.com/ramya-rao-a/go-outline FAILED
+Installing github.com/uudashr/gopkgs/cmd/gopkgs FAILED
+Installing github.com/acroca/go-symbols FAILED
+Installing github.com/ramya-rao-a/go-outline FAILED
+Installing golang.org/x/tools/cmd/guru FAILED
+Installing github.com/acroca/go-symbols FAILED
+Installing golang.org/x/tools/cmd/gorename FAILED
+Installing golang.org/x/tools/cmd/guru FAILED
+Installing github.com/cweill/gotests/... FAILED
+Installing golang.org/x/tools/cmd/gorename FAILED
+Installing github.com/fatih/gomodifytags FAILED
+Installing github.com/cweill/gotests/... FAILED
+Installing github.com/josharian/impl FAILED
+Installing github.com/fatih/gomodifytags FAILED
+Installing github.com/davidrjenni/reftools/cmd/fillstruct FAILED
+Installing github.com/josharian/impl FAILED
+Installing github.com/haya14busa/goplay/cmd/goplay FAILED
+Installing github.com/davidrjenni/reftools/cmd/fillstruct FAILED
+Installing github.com/godoctor/godoctor FAILED
+Installing github.com/haya14busa/goplay/cmd/goplay FAILED
+Installing github.com/stamblerre/gocode FAILED
+Installing github.com/godoctor/godoctor FAILED
+Installing github.com/rogpeppe/godef FAILED
+Installing github.com/stamblerre/gocode FAILED
+Installing github.com/sqs/goreturns FAILED
+Installing github.com/rogpeppe/godef FAILED
+Installing golang.org/x/lint/golint FAILED
+
+15 tools failed to install.
+-------------------------------------------------
+Failed download items:
+Installing github.com/uudashr/gopkgs/cmd/gopkgs FAILED
+Installing golang.org/x/tools/cmd/guru FAILED
+Installing golang.org/x/tools/cmd/gorename FAILED
+https://github.com/davidrjenni/reftools/cmd/fillstruct --> Not Found
+github.com/haya14busa/goplay/cmd/goplay --> Not Found
+Installing golang.org/x/lint/golint FAILED
+-----------------------------------------------------------------------
+D:\GoPath\src\github.com>
+git clone git@github.com:ramya-rao-a/go-outline.git
+git clone git@github.com:acroca/go-symbols.git
+git clone git@github.com:fatih/gomodifytags
+git clone git@github.com:cweill/gotests.git
+git clone git@github.com:josharian/impl.git
+git clone git@github.com:godoctor/godoctor.git
+git clone git@github.com:rogpeppe/godef.git
+git clone git@github.com:stamblerre/gocode.git
+git clone git@github.com:sqs/goreturns.git
+
+Installing github.com/acroca/go-symbols FAILED
+Installing github.com/ramya-rao-a/go-outline FAILED
+Installing golang.org/x/tools/cmd/guru FAILED
+Installing github.com/acroca/go-symbols FAILED
+Installing golang.org/x/tools/cmd/gorename FAILED
+Installing golang.org/x/tools/cmd/guru FAILED
+Installing github.com/cweill/gotests/... FAILED
+Installing golang.org/x/tools/cmd/gorename FAILED
+Installing github.com/fatih/gomodifytags FAILED
+Installing github.com/cweill/gotests/... FAILED
+Installing github.com/josharian/impl FAILED
+Installing github.com/fatih/gomodifytags FAILED
+Installing github.com/davidrjenni/reftools/cmd/fillstruct FAILED
+Installing github.com/josharian/impl FAILED
+Installing github.com/haya14busa/goplay/cmd/goplay FAILED
+Installing github.com/davidrjenni/reftools/cmd/fillstruct FAILED
+Installing github.com/godoctor/godoctor FAILED
+Installing github.com/haya14busa/goplay/cmd/goplay FAILED
+Installing github.com/stamblerre/gocode FAILED
+Installing github.com/godoctor/godoctor FAILED
+Installing github.com/rogpeppe/godef FAILED
+Installing github.com/stamblerre/gocode FAILED
+Installing github.com/sqs/goreturns FAILED
+Installing github.com/rogpeppe/godef FAILED
+Installing golang.org/x/lint/golint FAILED
